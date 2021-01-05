c051b26d01b38a563c435a018a1e9406d5812107
rafael2s <rafael_popo@hotmail.com>
2021-01-05 15:56:08 -0300

    03-Criando um cliente

--- FetchAPI/api/cliente.js
+++ FetchAPI/api/cliente.js
@@ -1,9 +1,32 @@
-listarClientes = () => {
+const listarClientes = () => {
     return fetch('http://localhost:4000/clientes')
     .then( resposta => {
         return resposta.json()
     })
     .then( json => {
         return json
+    })
+}
+
+const cadastrarCliente = (nome, cpf) => {
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf
+    })
+    return fetch('http://localhost:4000/clientes/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: json
+    })
+    .then(resp => {
+        return resp.body
+    })
+}
+
+const deletaCliente = id => {
+    return fetch(`http://localhost:4000/clientes/cliente/${id}`, {
+        method: 'DELETE'
     })
 }
--- FetchAPI/listagem-cliente.js
+++ FetchAPI/listagem-cliente.js
@@ -1,13 +1,19 @@
+const removeCliente = id => {
+    if(confirm("Deseja deleter o cliente?")){
+        deletaCliente(id)
+    }
+}
 
 const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 
-const exibeCliente = (cpf, nome) => {
+const exibeCliente = (cpf, nome, id) => {
 
     const linha = document.createElement('tr');
 
     const conteudoLinha = `
             <td>${cpf}</td>
             <td>${nome}</td>
+            
     `
     linha.innerHTML = conteudoLinha;
     return linha;
@@ -15,6 +21,6 @@ const exibeCliente = (cpf, nome) => {
 
 listarClientes().then( exibe => {
     exibe.forEach( indice => {
-        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
+        corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
     })
 })
